cc2440388e63533a1d86e3020bd5ad1d06ddf443
Janne Saarela <janne.juhani@gmail.com>
2014-07-07 22:10:09 +0300

    Edit main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
   $_(window).load(function(){
     // var transition_effect = slider_values.transition_effect || 'scrollLeft', transition_delay = slider_values.transition_delay || 9999999999, transition_duration = slider_values.transition_duration || 500;
     try{
-        $_('.slider-cycle').cycle({
+      $_('.slider-cycle').cycle({
         fx: 'scrollLeft',
         pager: '#controllers',
         activePagerClass: 'active',
@@ -25,60 +25,8 @@
       });
     } catch (e){}
   });
-
   $_(window).resize(function() {
     var newHeight = $_(".slider-cycle .displayblock").height();
     $_(".slider-cycle").css("height", newHeight);
   });
 })();
-
-/* Mobile menu
-/*! http://tinynav.viljamis.com v1.1 by @viljamis */
-(function ($, window, i) {
-  $.fn.tinyNav = function (options) {
-    var settings = $.extend({
-      'active': 'current-menu-item',
-      'header': false
-    }, options);
-    var counter = -1;
-    return this.each(function () {
-      i++;
-      var $nav = $(this),
-        namespace = 'tinynav',
-        namespace_i = namespace + i,
-        l_namespace_i = '.l_' + namespace_i,
-        $select = $('<select/>').addClass(namespace + ' ' + namespace_i);
-      if ($nav.is('ul,ol')) {
-        if (settings.header) {
-          $select.append($('<option/>').text('Valikko'));
-        }
-        var options = '';
-        $nav.addClass('l_' + namespace_i).find('a').each(function () {
-          options += '<option value="' + $(this).attr('href') + '">';
-          var j;
-          for (j = 0; j < $(this).parents('ul, ol').length - 1; j++) {
-            options += '- '
-          }
-          options += $(this).text() + '</option>'
-
-        });
-        $select.append(options);
-        if (!settings.header) {
-          $select.find(':eq(' + $(l_namespace_i + ' li').index($(l_namespace_i + ' li.' + settings.active)) + ')').attr('selected', true)
-        }
-        $select.change(function () {
-          window.location.href = $(this).val()
-        });
-        $(l_namespace_i).after($select);
-        if (settings.label) {
-          $select.before($("<label/>").attr("for", namespace_i).addClass(namespace + '_label ' + namespace_i + '_label').append(settings.label))
-        }
-      }
-    })
-  }
-})(jQuery, this, 0);
-jQuery(function () {
-  jQuery('#main-nav .menu-items').tinyNav({
-    active: 'current-menu-item'
-  })
-});
