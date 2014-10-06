87a40b2dcc337597ee93ca9f3e6b3aee6054b0dc
Janne Saarela <janne.juhani@gmail.com>
2014-10-06 13:45:37 +0300

    Add Instagram transient when get data

--- instagram/get-instagram-media.php
+++ instagram/get-instagram-media.php
@@ -9,49 +9,81 @@ if ( !defined('ABSPATH')) exit;
  * @file           get-instagram-media.php
  * @package        Touko The Politician
  * @author         Janne Saarela
- * @version        Release: 1.0
+ * @version        Release: 0.9.0
  * @filesource     wp-content/themes/touko-the-politician/instagram/get-instagram-media.php
- * @since          available since Release 1.0
+ * @since          available since Release 0.9.0
  */
-  require_once(get_stylesheet_directory() . '/vendor/instagram/instagram.php');
 
-  global $touko_the_politician_theme_options_settings, $instagram_media, $master_instagram;
-  $theme_settings = $touko_the_politician_theme_options_settings;
-  $instagram_posts_count = $theme_settings['instagram_visible_posts_count'];
 
-  $auth_config = array(
-    'apiKey'         => $theme_settings['instagram_api_key'],
-    'apiSecret'     => $theme_settings['instagram_api_secret'],
-    'apiCallback'      => $theme_settings['instagram_api_callback']
-  );
-  if(gettype($master_instagram) !== 'object') :
-    try{
-      $master_instagram = new Instagram($auth_config);
-    }
-    catch(Exception $e){
-      error_log(date('j.n.Y H:i:s'). " : ", 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-      error_log($e.PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-      error_log("-----".PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-    }
-  endif;
-  if (get_option('instagram-access-token') !== false) :
-    try{
-      $instagram = $master_instagram;
-      $user = $instagram -> searchUser($theme_settings['instagram_username']);
-      // echo "<pre>";
-      // print_r($user);
-      // echo "</pre>";
-      if(gettype($user) !== 'NULL'){
-        $user = (array)$user;
-        $user_id = $user['data'][0]->id;
-        $instagram->setAccessToken(get_option('instagram-access-token'));
-        $instagram_media = $instagram->getUserMedia($user_id, $instagram_posts_count);
+if ( !function_exists('get_instagram_data') ) :
+
+  /**
+  * Get data from Instagram API
+  */
+
+  function get_instagram_data(){
+    require_once( get_stylesheet_directory() . '/vendor/instagram/instagram.php' );
+    global $touko_the_politician_theme_options_settings, $instagram_media, $master_instagram;
+    $theme_settings = $touko_the_politician_theme_options_settings;
+    $instagram_posts_count = $theme_settings['instagram_visible_posts_count'];
+
+    $auth_config = array(
+      'apiKey'         => $theme_settings['instagram_api_key'],
+      'apiSecret'     => $theme_settings['instagram_api_secret'],
+      'apiCallback'      => $theme_settings['instagram_api_callback']
+    );
+
+    if( gettype($master_instagram) !== 'object' ) :
+      try{
+        $master_instagram = new Instagram( $auth_config );
       }
-    }
-    catch(Exception $e){
-     error_log(date('j.n.Y H:i:s'). " : ", 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-      error_log($e.PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-      error_log("-----".PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
-    }
-  endif;
+      catch(Exception $e){
+        error_log(date('j.n.Y H:i:s'). " : ", 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+        error_log($e.PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+        error_log("-----".PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+      }
+    endif;
+
+    if ( get_option('instagram-access-token') !== false ) :
+      try{
+        $instagram = $master_instagram;
+        $user = $instagram -> searchUser($theme_settings['instagram_username']);
+        // echo "<pre>";
+        // print_r($user);
+        // echo "</pre>";
+        if(gettype($user) !== 'NULL'){
+          $user = (array)$user;
+          $user_id = $user['data'][0]->id;
+          $instagram->setAccessToken(get_option('instagram-access-token'));
+          $instagram_media = $instagram->getUserMedia($user_id, $instagram_posts_count);
+          return $instagram_media;
+        }
+      }
+      catch(Exception $e){
+       error_log(date('j.n.Y H:i:s'). " : ", 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+        error_log($e.PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+        error_log("-----".PHP_EOL, 3, get_stylesheet_directory() .'/logs/instagram-errors.log');
+      }
+    endif;
+  }
+
+endif;
+
+if ( !function_exists('instagram_transient') ) :
+/**
+ * set Instagram transient
+ *
+ * @uses set_transient
+ */
+function instagram_transient() {
+  if( !get_transient('instagram_transient') ) {
+    $instagram_transient = get_instagram_data();
+    // Set 15 min cache
+    set_transient('instagram_transient', $instagram_transient, 900);
+  }
+}
+endif;
+
+instagram_transient();
+
 ?>
--- instagram/instagram-media.php
+++ instagram/instagram-media.php
@@ -9,63 +9,71 @@ if ( !defined('ABSPATH')) exit;
  * @file           instagram-media.php
  * @package        Touko The Politician
  * @author         Janne Saarela
- * @version        Release: 1.0
+ * @version        Release: 0.9.0
  * @filesource     wp-content/themes/touko-the-politician/instagram/instagram-media.php
- * @since          available since Release 1.0
+ * @since          available since Release 0.9.0
  */
 ?>
 
 <?php
-  global $instagram_media, $touko_the_politician_theme_options_settings;
-  $theme_settings = $touko_the_politician_theme_options_settings;
-  $data = $instagram_media;
-  if (gettype($data) !== 'NULL') :
-    $data = (array)$instagram_media;
-    // echo "<pre>";
-    // print_r($data["data"][0]["images"]);
-    // echo "</pre>";
-    ?>
-    <?php
-    foreach( $data['data'] as $key => $value ) { ?>
-      <article class="instagram-media-post instagram-post-<?php echo $key;?> grid-50-with-gap clearfix">
-        <section class="instagram-user-details clearfix">
-          <figure class="newsfeed-icon-img instagram-page-img clearfix">
-            <img src="<?php echo $value->user->profile_picture;?>" alt="<?php echo $value->user->full_name;?>">
-          </figure>
-          <a href="<?php echo esc_url('https://www.instagram.com/'.$value->user->username);?>" title="<?php echo $value->user->full_name;?> @ Instagram">
-            <h3><?php echo $value->user->full_name; ?></h3>
+
+global $instagram_media, $touko_the_politician_theme_options_settings;
+$theme_settings = $touko_the_politician_theme_options_settings;
+$data = $instagram_media;
+
+if ( !get_transient( 'instagram_transient') ) {
+  call_user_func( 'instagram_transient' );
+}
+
+$instagram_data = get_transient( 'instagram_transient' );
+
+if (gettype($instagram_data) !== 'NULL') :
+  $data = (array)$instagram_data;
+  // echo "<pre>";
+  // print_r($data["data"][0]);
+  // echo "</pre>";
+  ?>
+  <?php
+  foreach( $data['data'] as $key => $value ) { ?>
+    <article class="instagram-media-post instagram-post-<?php echo $key;?> grid-50-with-gap clearfix">
+      <section class="instagram-user-details clearfix">
+        <figure class="newsfeed-icon-img instagram-page-img clearfix">
+          <img src="<?php echo $value->user->profile_picture;?>" alt="<?php echo $value->user->full_name;?>">
+        </figure>
+        <a href="<?php echo esc_url('https://www.instagram.com/'.$value->user->username);?>" title="<?php echo $value->user->full_name;?> @ Instagram">
+          <h3><?php echo $value->user->full_name; ?></h3>
+        </a>
+      </section>
+      <section class="instagram-media-details">
+        <figure class="instagram-img">
+          <a href="<?php echo $value->link;?>" title="<?php echo $value->link;?>">
+            <img src="<?php echo $value->images->thumbnail->url;?>" alt="thumbnail">
           </a>
-        </section>
-        <section class="instagram-media-details">
-          <figure class="instagram-img">
-            <a href="<?php echo $value->link;?>" title="<?php echo $value->link;?>">
-              <img src="<?php echo $value->images->thumbnail->url;?>" alt="thumbnail">
-            </a>
+        </figure>
+        <?php
+          if (isset($value->caption->text)) : ?>
+          <figure class="newsfeed-icon instagram-logo">
+            <i class="icon-instagram"></i>
           </figure>
-          <?php
-            if (isset($value->caption->text)) : ?>
-            <figure class="newsfeed-icon instagram-logo">
-              <i class="icon-instagram"></i>
-            </figure>
-            <p class="instagram-media-text"><?php echo $value->caption->text;?></p>
-          <?php
-            endif;
-           ?>
-          <?php
-            $datetime = date($value->created_time);
-            // Set datetime timezone match to timezone set in wp
-            $timezone_offset = get_option('gmt_offset');
-            $datetime = $datetime+(60*60*$timezone_offset);
-          ?>
-          <span class="datetime"><?php echo gmdate('d.m.Y H:i', $datetime);?></span>
-        </section>
-      </article>
-      <?php
-      // add clearfix after every two images's
-        if ($key % 2 === 1 && $theme_settings['instagram_visible_posts_count'] > 2) { ?>
-          <hr class="clearfix">
-        <?php }
-    }
+          <p class="instagram-media-text"><?php echo $value->caption->text;?></p>
+        <?php
+          endif;
+         ?>
+        <?php
+          $datetime = date($value->created_time);
+          // Set datetime timezone match to timezone set in wp
+          $timezone_offset = get_option('gmt_offset');
+          $datetime = $datetime+(60*60*$timezone_offset);
+        ?>
+        <span class="datetime"><?php echo gmdate('d.m.Y H:i', $datetime);?></span>
+      </section>
+    </article>
+    <?php
+    // add clearfix after every two images's
+      if ($key % 2 === 1 && $theme_settings['instagram_visible_posts_count'] > 2) { ?>
+        <hr class="clearfix">
+      <?php }
+  }
     ?>
 <?php else : // If Instagram data isnt available  ?>
   <article class="instagram-media-post grid-100 clearfix">
